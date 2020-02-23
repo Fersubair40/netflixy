1090e7911d389fca2e29bef27dc799f8d5a98c09
fersubair40 <olajnrsubair@gmail.com>
2020-02-23 10:34:53 +0100

    update

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,31 +1,35 @@
 import React, { Fragment } from 'react';
-import { movies } from '../staticData';
 import FontAwesomeIcon from './FontAwesomeIcon';
 import StyleHeader from './StyleHeader';
 import StyleHeaderTitle from './StyleHeaderTitle';
+import { movies } from '../staticData';
 import StyledHorizontalScroll from './StyledHorizontalScroll';
+import Movie from './Movie';
+// import StyledFooter from './StyledFooter';
+// import StyledLargeBtn from './StyledLargeBtn';
 
 const Movies = () => (
 	<Fragment>
 		<StyleHeader>
 			<FontAwesomeIcon icon="bars" text="help" />
-			<StyleHeaderTitle> The Movie Recommender</StyleHeaderTitle>
+			<StyleHeaderTitle>The Movie Recommender</StyleHeaderTitle>
 			<FontAwesomeIcon icon="search" />
 		</StyleHeader>
-
+		{/* the list of movies */}
 		<StyledHorizontalScroll>
 			{movies.map(movie => (
-				<div
-					style={{
-						border: '1px solid red',
-						margin: '1rem',
-					}}
+				<Movie
 					key={movie.id}
-				>
-					{movie.name}
-				</div>
+					name={movie.name}
+					poster={movie.poster}
+					duration={movie.duration}
+					year={movie.year}
+				/>
 			))}
 		</StyledHorizontalScroll>
+		{/* <StyledFooter>
+			<StyledLargeBtn>Get Recommended Movies</StyledLargeBtn>
+		</StyledFooter> */}
 	</Fragment>
 );
 
---
+++ src/components/StyledMovie.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const StyledMovie = styled.div`
+	padding: 0.6rem;
+`;
+export default StyledMovie;
